758d8d4e1e46020910dd4b07b3f457b87a25fa64
Tommy Miland <tmiland@gmail.com>
2014-03-17 11:46:21 +0100

    change structure

---
+++ Punish Explosives/no_explosives_warning_on_spawn.cs
@@ -0,0 +1,36 @@
+/* Version 9.16/R1a */
+/* SETUP */
+
+// Messages
+String msg = "WARNING: You will be AUTO-KICKED if you use EXPLOSIVES!";
+String yellMsg = @"WARNING: You will be AUTO-KICKED if you use EXPLOSIVES!";
+String msg1 = "WARNING: You will be AUTO-KICKED if you use EXPLOSIVES!";
+String msg2 = "Please type @rules in chat to see the Server Rules.";
+
+// Times
+int yellTime = 5; // seconds
+int secondNoticeSpawnCount = 3;
+
+/* CODE */
+
+String key = "WelcomeRulesExplosives_" + player.Name;
+
+int count = 0;
+if (player.Data.issetInt(key)) count = player.Data.getInt(key);
+
+count = count + 1;
+
+if (count == 1) { // First notice
+
+    plugin.SendPlayerMessage(player.Name, msg);
+    plugin.SendPlayerYell(player.Name, yellMsg, yellTime);
+
+} else if (count == secondNoticeSpawnCount) { // Second notice
+
+    plugin.SendPlayerMessage(player.Name, msg1);
+    plugin.SendPlayerMessage(player.Name, msg2);
+}
+
+player.Data.setInt(key, count);
+
+return false;
