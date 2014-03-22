0684253a4702477ace6685ee1c9e66561b80a97d
Tommy Miland <tmiland@gmail.com>
2014-03-22 16:29:12 +0100

    yellTime changed from 20 to 25 sec

--- No AA/aa_notices.cs
+++ No AA/aa_notices.cs
@@ -7,12 +7,12 @@ first check code: */
 
 // Messages
 String msg = "WARNING: You will be AUTO-KICKED if you use the PROHIBITED Mobile-AA or AA-Mine!";
-String yellMsg = @"WARNING: You will be AUTO-KICKED                                                                      if you use the PROHIBITED Mobile-AA or AA-Mine!";
+String yellMsg = @"WARNING: You will be AUTO-KICKED if you use the PROHIBITED Mobile-AA or AA-Mine!";
 String msg1 = "WARNING: You will be AUTO-KICKED if you use the PROHIBITED Mobile-AA or AA-Mine!";
 String msg2 = "Please type @rules in chat to see the Server Rules.";
 
 // Times
-int yellTime = 20; // seconds
+int yellTime = 25; // seconds
 int secondNoticeSpawnCount = 3;
 
 /* CODE */
