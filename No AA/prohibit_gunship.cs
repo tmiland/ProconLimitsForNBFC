bcacad63e99379445bcb1bdbceb994e4e629cb47
Tommy Miland <tmiland@gmail.com>
2014-04-02 16:02:43 +0200

    No Gunship limit

---
+++ No AA/prohibit_gunship.cs
@@ -0,0 +1,73 @@
+/* https://forum.myrcon.com/showthread.php?7617-Insane-Limits-Mobile-Anti-Air-and-AA-Mine-Limit
+Prohibit Gunship
+OnKill
+first check code */
+/* Version 9.16/R4 */
+/* SETUP */
+
+// Message templates
+// {0} will be replaced with PlayerName
+// {1} will be replaced by prohibited weapon/vehicle name, see below
+String autoKilled = "{0} AUTO-KILLED for using the PROHIBITED {1}";
+String autoKicked = "{0} AUTO-KICKED for using the PROHIBITED {1}";
+String tempBan = "{0} TEMP BAN 1 HOUR for using the PROHIBITED {1}";
+String yellKilled = "The {1} is prohibited. You will be AUTO-KICKED if you use it again.";
+
+// Times
+int yellTime = 25; // seconds
+int banTime = 60; // minutes
+double multiKillTime = 5; // seconds
+
+// Weapon/vehicle codes
+
+bool isGunship = Regex.Match(kill.Weapon, @"(AC130_Gunship)").Success;
+
+/* CODE */
+
+if (!isGunship) return false;
+
+String prohibited = "Gunship";
+
+String key = "PersistGunship_" + prohibited + "_" + killer.Name;
+
+int count = 0;
+if (plugin.Data.issetInt(key)) count = plugin.Data.getInt(key);
+
+DateTime last = DateTime.MinValue;
+if (server.Data.issetObject(key)) last = (DateTime)server.Data.getObject(key);
+if (DateTime.Now.Subtract(last).TotalSeconds <= multiKillTime) return false;
+server.Data.setObject(key, (Object)DateTime.Now);
+
+count = count + 1;
+
+String msg = null;
+
+if (count == 1) { // First Violation: Kill
+
+    msg = String.Format(autoKilled, killer.Name, prohibited);
+    plugin.SendGlobalMessage(msg);
+    plugin.SendPlayerYell(killer.Name, String.Format(yellKilled, killer.Name, prohibited), yellTime);
+    plugin.PRoConChat("ADMIN > " + msg);
+    plugin.PRoConEvent(msg, "Insane Limits");
+    plugin.KillPlayer(killer.Name, 6);
+
+} else if (count == 2) { // Second Violation: Kick
+
+    msg = String.Format(autoKicked, killer.Name, prohibited);
+    plugin.SendGlobalMessage(msg);
+    plugin.PRoConChat("ADMIN > " + msg);
+    plugin.PRoConEvent(msg, "Insane Limits");
+    plugin.KickPlayerWithMessage(killer.Name, msg);
+
+} else { // Third or subsequent Violation: TBan
+
+    msg = String.Format(tempBan, killer.Name, prohibited);
+    plugin.SendGlobalMessage(msg);
+    plugin.PRoConChat("ADMIN > " + msg);
+    plugin.PRoConEvent(msg, "Insane Limits");
+    plugin.EABanPlayerWithMessage(EABanType.Name, EABanDuration.Temporary, killer.Name, banTime, msg);
+
+}
+
+plugin.Data.setInt(key, count);
+return false;
