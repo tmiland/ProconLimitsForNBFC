25fd5a4198883754cd2e2a064260216bb9837eac
Tommy Miland <tmiland@gmail.com>
2014-04-07 20:33:16 +0200

    Updated for use of bad words list

--- Mute on Profanity/Mute on Profanity.cs
+++ Mute on Profanity/Mute on Profanity.cs
@@ -3,28 +3,14 @@ Set limit to evaluate OnAnyChat, set action to None
 
 Set first_check to this Code: */
 
-	List<String> bad_words = new List<String>();
-	//bad_words.Add("BadWord"); // Change This
-	// this matches nigger, n1g3r, nig3, etc 
-	bad_words.Add(@"n+[1i]+g+[3ea]+r*"); // Change This
-	// this matches faggot, f4g0t, fagat, etc
-	bad_words.Add(@"f+[a4]+g+[ao0]+t*"); // Change This
-	// this matches fag, f4g, fagggg, fa4g, etc.
-	bad_words.Add(@"f+[a4]+g+"); // Change This
-	// this matches gay, g4y, gayyyy, ga4y, etc.
-	bad_words.Add(@"g+[a4]+y+"); // Change This
-	// this matches jew, j3w, jeeeew, je3w, etc.
-	bad_words.Add(@"j+[e3]+w+"); // Change This
-	bad_words.Add(@"dick*"); // Change This
-	bad_words.Add(@"cock*"); // Change This
+	string[] chat_words = Regex.Split(player.LastChat, @"\s+");
 
-	String[] chat_words = Regex.Split(player.LastChat, @"\s+");
-
-	foreach(String chat_word in chat_words)
-		foreach(String bad_word in bad_words)
-			if (Regex.Match(chat_word, "^"+bad_word+"$", RegexOptions.IgnoreCase).Success)
-				return true;
-
+	foreach(string chat_word in chat_words)
+	{
+		if (plugin.isInList(chat_word, "bad_words_list")) {
+			return true;
+		}
+	}
 	return false;
 
 	/* Set second_check to this Code: */
