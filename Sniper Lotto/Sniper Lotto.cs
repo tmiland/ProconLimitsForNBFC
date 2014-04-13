ca67f064106a3f5e81296b8e4e611ccfacc7d0bf
Tommy Miland <tmiland@gmail.com>
2014-04-13 11:26:59 +0200

    some changes

--- Sniper Lotto/Sniper Lotto.cs
+++ Sniper Lotto/Sniper Lotto.cs
@@ -10,8 +10,8 @@ int maxSnipers = 5; // Number of snipers per team to choose from the lottery
 String msg = "test";
 
 Action<String> ChatPlayer = delegate(String who) {
-    plugin.ServerCommand("admin.yell", msg, "15", "player", who);
-    plugin.ServerCommand("admin.say", msg, "player", who);
+	plugin.ServerCommand("admin.yell", msg, "15", "player", who);
+	plugin.ServerCommand("admin.say", msg, "player", who);
 };
 
 // Use kills to update the lotto time limit
@@ -29,104 +29,105 @@ int state = 0;
 if (plugin.RoundData.issetInt(kState)) state = plugin.RoundData.getInt(kState);
 
 bool sniperRifleUsed = (kill.Category == "SniperRifle" || kill.Category == "DMR");
+if (kill.Category == "Handgun" || kill.Category == "Explosive" || kill.Weapon == "Melee") return false;
 
 // State 0: Waiting for first player to type the !sniper command
 if (state == 0) {
-    if (!sniperRifleUsed) return false;
-    // Punish and remind
-    msg = "No sniping until after lottery: type !sniper to enter the lottery";
-    ChatPlayer(killer.Name); // Remind player
-    plugin.KillPlayer(killer.Name, 5);
-    plugin.ServerCommand("admin.yell", msg, "8"); // Remind everyone
-    return false;
+	if (!sniperRifleUsed) return false;
+	// Punish and remind
+	msg = "No sniping until after lottery: type !sniper to enter the lottery";
+	ChatPlayer(killer.Name); // Remind player
+	plugin.KillPlayer(killer.Name, 5);
+	plugin.ServerCommand("admin.yell", msg, "8"); // Remind everyone
+	return false;
 }
 
 // State 1: Running the lotto for maxMinutes
 if (state == 1) {
-    DateTime start = DateTime.Now;
-    if (!plugin.RoundData.issetObject(kTime)) {
-        plugin.RoundData.setObject(kTime, (Object)start);
-    } else {
-        start = (DateTime)plugin.RoundData.getObject(kTime);
-    }
-    TimeSpan since = DateTime.Now.Subtract(start);
-        if (((int)since.TotalSeconds) % 10 == 0) plugin.ConsoleWrite("^b[Lotto]^n Lottery has been running for " + since.TotalSeconds.ToString("F0") + " seconds ...");
-    if (since.TotalMinutes < maxMinutes) {
-        if (!sniperRifleUsed) return false;
-        // Punish and remind
-        msg = "No sniping until after lottery: type !sniper to enter the lottery";
-        ChatPlayer(killer.Name); // Remind player
-        plugin.KillPlayer(killer.Name, 5);
-        plugin.ServerCommand("admin.yell", msg, "8"); // Remind everyone
-        return false;
-    }
-    if (!plugin.RoundData.issetObject(kEntries1) && !plugin.RoundData.issetObject(kEntries2)) {
-        plugin.ConsoleWrite("^b[Lotto]^n no entrants and time has expired!");
-        plugin.RoundData.setInt(kState, 2);
-        return false;
-    }
-    List<String> entrants1 = new List<String>();
-        if (plugin.RoundData.issetObject(kEntries1)) entrants1 = (List<String>)plugin.RoundData.getObject(kEntries1);
-    List<String> entrants2 = new List<String>();
-        if (plugin.RoundData.issetObject(kEntries2)) entrants2 = (List<String>)plugin.RoundData.getObject(kEntries2);
-    if (entrants1.Count == 0 && entrants2.Count == 0)  {
-        plugin.ConsoleWrite("^b[Lotto]^n no entrants and time has expired!");
-        plugin.RoundData.setInt(kState, 2);
-        return false;
-    }
-    msg = "The lottery is over! The winners are:";
-    plugin.SendGlobalMessage(msg);
-    Random rnd = new Random();
-    int pick = maxSnipers;
-    // Team 1
-    msg = ": may use sniper rifles this round!";
-    while (pick > 0 && entrants1.Count > 0) {
-        int winner = (entrants1.Count > 2) ? rnd.Next(entrants1.Count) : 0;
-        String name = entrants1[winner];
-        entrants1.RemoveAt(winner);
-        key = kPrefix + name;
-        plugin.RoundData.setBool(key, true);
-        msg = name + msg;
-        --pick;
-        if (pick > 0) msg = ", " + msg;
-    }
-    // Team 2
-    pick = maxSnipers;
-    if (entrants2.Count > 0) msg = ", " + msg;
-    while (pick > 0 && entrants2.Count > 0) {
-        int winner = (entrants2.Count > 2) ? rnd.Next(entrants2.Count) : 0;
-        String name = entrants2[winner];
-        entrants2.RemoveAt(winner);
-        key = kPrefix + name;
-        plugin.RoundData.setBool(key, true);
-        msg = name + msg;
-        --pick;
-        if (pick > 0) msg = ", " + msg;
+	DateTime start = DateTime.Now;
+	if (!plugin.RoundData.issetObject(kTime)) {
+		plugin.RoundData.setObject(kTime, (Object)start);
+	} else {
+		start = (DateTime)plugin.RoundData.getObject(kTime);
+	}
+	TimeSpan since = DateTime.Now.Subtract(start);
+		if (((int)since.TotalSeconds) % 10 == 0) plugin.ConsoleWrite("^b[Lotto]^n Lottery has been running for " + since.TotalSeconds.ToString("F0") + " seconds ...");
+	if (since.TotalMinutes < maxMinutes) {
+		if (!sniperRifleUsed) return false;
+		// Punish and remind
+		msg = "No sniping until after lottery: type !sniper to enter the lottery";
+		ChatPlayer(killer.Name); // Remind player
+		plugin.KillPlayer(killer.Name, 5);
+		plugin.ServerCommand("admin.yell", msg, "8"); // Remind everyone
+		return false;
+	}
+	if (!plugin.RoundData.issetObject(kEntries1) && !plugin.RoundData.issetObject(kEntries2)) {
+		plugin.ConsoleWrite("^b[Lotto]^n no entrants and time has expired!");
+		plugin.RoundData.setInt(kState, 2);
+		return false;
+	}
+	List<String> entrants1 = new List<String>();
+		if (plugin.RoundData.issetObject(kEntries1)) entrants1 = (List<String>)plugin.RoundData.getObject(kEntries1);
+	List<String> entrants2 = new List<String>();
+		if (plugin.RoundData.issetObject(kEntries2)) entrants2 = (List<String>)plugin.RoundData.getObject(kEntries2);
+	if (entrants1.Count == 0 && entrants2.Count == 0)  {
+		plugin.ConsoleWrite("^b[Lotto]^n no entrants and time has expired!");
+		plugin.RoundData.setInt(kState, 2);
+		return false;
+	}
+	msg = "The lottery is over! The winners are:";
+	plugin.SendGlobalMessage(msg);
+	Random rnd = new Random();
+	int pick = maxSnipers;
+	// Team 1
+	msg = ": may use sniper rifles this round!";
+	while (pick > 0 && entrants1.Count > 0) {
+		int winner = (entrants1.Count > 2) ? rnd.Next(entrants1.Count) : 0;
+		String name = entrants1[winner];
+		entrants1.RemoveAt(winner);
+		key = kPrefix + name;
+		plugin.RoundData.setBool(key, true);
+		msg = name + msg;
+		--pick;
+		if (pick > 0) msg = ", " + msg;
+	}
+	// Team 2
+	pick = maxSnipers;
+	if (entrants2.Count > 0) msg = ", " + msg;
+	while (pick > 0 && entrants2.Count > 0) {
+		int winner = (entrants2.Count > 2) ? rnd.Next(entrants2.Count) : 0;
+		String name = entrants2[winner];
+		entrants2.RemoveAt(winner);
+		key = kPrefix + name;
+		plugin.RoundData.setBool(key, true);
+		msg = name + msg;
+		--pick;
+		if (pick > 0) msg = ", " + msg;
     }
     plugin.SendGlobalMessage(msg);
     plugin.ServerCommand("admin.yell", msg, "15");
     plugin.ConsoleWrite("^b[Lotto]^n " + msg);
-        plugin.RoundData.setString(kWinners, msg);
+    plugin.RoundData.setString(kWinners, msg);
 
     plugin.RoundData.setInt(kState, 2);
-    return false;
+	return false;
 }
 
 // State 2: Lottery is over, only winners may use sniper rifles
 if (state == 2) {
-    if (sniperRifleUsed && !plugin.RoundData.issetBool(key)) {
-        msg = killer.Name + ": You are not allowed to use sniper rifles this round!";
-        ChatPlayer(killer.Name);
-        plugin.KillPlayer(killer.Name, 5);
-        return false;    
-    }
-	    if (!sniperRifleUsed && plugin.RoundData.issetBool(key)) {
-        msg = killer.Name + ": You entered and won the lottery, you MUST use sniper rifles this round!";
-        ChatPlayer(killer.Name);
-        plugin.KillPlayer(killer.Name, 5);
-        // plugin.KickPlayerWithMessage(killer.Name, "You entered and won the lottery, you MUST use sniper rifles!");
-        return false;    
-    }
+	if (sniperRifleUsed && !plugin.RoundData.issetBool(key)) {
+		msg = killer.Name + ": You are not allowed to use sniper rifles this round!";
+		ChatPlayer(killer.Name);
+		plugin.KillPlayer(killer.Name, 5);
+		return false;    
+	}
+	if (!sniperRifleUsed && plugin.RoundData.issetBool(key)) {
+		msg = killer.Name + ": You entered and won the lottery, you MUST use sniper rifles this round!";
+		ChatPlayer(killer.Name);
+		plugin.KillPlayer(killer.Name, 5);
+		// plugin.KickPlayerWithMessage(killer.Name, "You entered and won the lottery, you MUST use sniper rifles!");
+		return false;    
+	}
 }
 
 return false;
