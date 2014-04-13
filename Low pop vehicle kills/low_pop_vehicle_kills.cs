51703350bd85046177c598219473db4be00eb92f
Tommy Miland <tmiland@gmail.com>
2014-04-13 11:26:04 +0200

    Changed to vehicle categories

--- Low pop vehicle kills/low_pop_vehicle_kills.cs
+++ Low pop vehicle kills/low_pop_vehicle_kills.cs
@@ -15,18 +15,18 @@ Note that adminKillCount and warningCount are forced to be less than kickCount. 
 Set first_check to this Code: */
 // Variables
 int minimumPlayers = 16; // you can change this number
-int warningCount = 2; // you can change this number
-int adminKillCount = 2; // you can change this number
-int kickCount = 3; // you can change this number
+int warningCount = 3; // you can change this number
+int adminKillCount = 4; // you can change this number
+int kickCount = 5; // you can change this number
 // You can change these messages also:
 String warningMessage = "Vehicle kills will be punished until there are more than " + minimumPlayers + " players!"; 
 String kickMessage = "ignored warnings about no vehicle kills";
 
-bool isVehicle = Regex.Match(kill.Weapon, @"(VehicleLight|VehicleHeavy|VehicleAir|VehicleWater)").Success;
+bool isVehicle = (kill.Category == "VehicleLight" || kill.Category == "VehicleHeavy" || kill.Category == "VehicleAir" || kill.Category == "VehicleWater");
 
 /* CODE */
-
 if (!isVehicle) return false;
+//if (!Regex.Match(kill.Weapon, @"(Death|RoadKill)", RegexOptions.IgnoreCase).Success) return false;
 if (killer.Name == victim.Name) return false;
 if (killer.Name == "Server") return false;
 if (server.PlayerCount > minimumPlayers) return false;
@@ -48,6 +48,7 @@ if (adminKillCount >= kickCount) adminKillCount = kickCount - 1;
 if (count >= kickCount) {
     String tmp = "Kicking " + killer.Name + " killed " + victim.Name + " with " + kill.Weapon + ", reason: " + kickMessage;
     plugin.ConsoleWrite(tmp);
+	plugin.SendGlobalMessage(kickMessage);
     plugin.PRoConChat("Insane Limits > " + tmp);
     plugin.KickPlayerWithMessage(killer.Name, kickMessage);
     return false;
@@ -56,6 +57,7 @@ if (count >= kickCount) {
 bool warned = false;
 if (count <= warningCount) {
     plugin.SendPlayerMessage(killer.Name, warningMessage);
+	plugin.SendGlobalMessage(warningMessage);
     plugin.PRoConChat("Insane Limits > " + killer.Name + ": " + warningMessage);
     warned = true;
 }
@@ -64,6 +66,7 @@ if (count <= warningCount) {
 if (count <= adminKillCount) {
     if (!warned) {
         plugin.SendPlayerMessage(killer.Name, warningMessage);
+		plugin.SendGlobalMessage(warningMessage);
         plugin.PRoConChat("Insane Limits > " + killer.Name + ": " + warningMessage);
     }
     plugin.KillPlayer(killer.Name, 5);
@@ -77,7 +80,7 @@ For each players first noticeCount spawns, this limit will announce that vehicle
 
 The value of minimumPlayers MUST be the same in both limits. If you change the code above, you must also change this code.
 
-Create a new limit OnSpawn, call it "Low Pop Spawn Notice", leave Action set to None.
+Create a new limit OnSpawn, call it "Low pop Spawn Notice", leave Action set to None.
 
 In the code that follows, these variables define how the limit works -- think of these like settings, but you have to change the code to change them:
 
@@ -87,14 +90,14 @@ noticeCount: first through this number of spawns will send a chat message to the
 */
 // Variables
 int minimumPlayers = 16; // you can change this number - MUST BE THE SAME AS THE OnKill LIMIT!
-int noticeCount = 2; // you can change this number
+int noticeCount = 4; // you can change this number
 // You can change this message also:
 String noticeMessage  = "Vehicle kills will be punished until there are more than " + minimumPlayers + " players!"; 
 
 // Code
 if (server.PlayerCount > minimumPlayers) return false;
 
-String prefix = "SpawnNotice_";
+String prefix = "LowPopSpawnNotice_";
 String key = prefix + player.Name;
 
 int count = 0;
@@ -105,6 +108,6 @@ if (count > noticeCount) return false;
 count = count + 1;
 
 plugin.RoundData.setInt(key, count);
-
+plugin.ServerCommand("admin.say", msg, "player", who);
 plugin.SendPlayerMessage(player.Name, noticeMessage);
 return false;
