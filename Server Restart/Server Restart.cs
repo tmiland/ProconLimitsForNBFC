4d25c113d3afa913a29c42b0e40b46f7a4d1f5e3
Tommy Miland <tmiland@gmail.com>
2014-05-01 12:12:16 +0200

    Server Restart Limit
    This will restart the server if uptime is 6h and empty.

---
+++ Server Restart/Server Restart.cs
@@ -0,0 +1,10 @@
+/* Server Restart - https://forum.myrcon.com/showthread.php?7817-Plugin-to-auto-reboot-server&p=102493&viewfull=1#post102493
+Set limit to evaluate OnIntervalServer, EvaluationInterval: 3600, set action to None
+
+Set first_check to this Expression: */
+server.TimeUp > 21600 && server.PlayerCount == 0
+
+/* Set second_check to this Code: */
+plugin.ServerCommand("admin.shutDown");
+return false;
+/* Note: Please make sure that your server is starting again after it was shut down */
