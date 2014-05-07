c1fae76c83d87582eb2b88df84f157846dd67dcb
Tommy Miland <tmiland@gmail.com>
2014-05-07 09:58:58 +0200

    Idle Kick on playercount

---
+++ Idle Kick/Idle Kick.cs
@@ -0,0 +1,12 @@
+int threshold = 32;
+int idleTimeout = 300;
+
+if (server.PlayerCount < threshold ) {
+plugin.ServerCommand("vars.idleTimeout", "86400");
+plugin.ConsoleWrite("^2idleTimeout: ^b86400^n");
+}
+else {
+plugin.ServerCommand("vars.idleTimeout", "idleTimeout.ToString()");
+plugin.ConsoleWrite("^2idleTimeout: ^b" + idleTimeout + "^n");
+}
+return false;
