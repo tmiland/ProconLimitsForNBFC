0c1d1d8c9c622e11857209fee7c6e99cedec62d5
Tommy Miland <tmiland@gmail.com>
2014-10-25 09:09:56 +0200

    Name change
    Name change and added more lists.

--- Mute on Bad Words/Mute on Bad Words.cs
+++ Mute on Bad Words/Mute on Bad Words.cs
@@ -1,4 +1,4 @@
-/* Mute on Profanity - https://forum.myrcon.com/showthread.php?7737-Insane-Limits-Mute-on-Profanity
+/* Mute on Bad Words - https://forum.myrcon.com/showthread.php?7737-Insane-Limits-Mute-on-Profanity
 Set limit to evaluate OnAnyChat, set action to None
 
 Set first_check to this Code: */
@@ -7,7 +7,9 @@ Set first_check to this Code: */
 
 	foreach(string chat_word in chat_words)
 	{
-		if (plugin.isInList(chat_word, "bad_words_list")) 
+		if (plugin.isInList(chat_word, "english_bad_words_list") |
+		(plugin.isInList(chat_word, "norwegian_bad_words_list")) |
+		(plugin.isInList(chat_word, "spanish_bad_words_list")))
 		{
 			return true;
 		}
@@ -21,20 +23,20 @@ Set first_check to this Code: */
 	double count = limit.Activations(player.Name);
 	String msg = "none";
 	if (count == 1) {
-		msg = plugin.R("ATTENTION %k_n%! Please avoid using profanity, you will be muted for the rest of the round!");
+		msg = plugin.R("ATTENTION %k_n%! Please avoid using bad language, you will be muted for the rest of the round!");
 		plugin.SendPlayerYell(player.Name, msg, 20);
 		plugin.SendGlobalMessage(msg);
 		plugin.PRoConChat("ADMIN MUTE > " + msg);
-		plugin.ConsoleWrite("^b^1ADMIN MUTE >^0^n " + player.FullName + " tripped the profanity filter! ");
+		plugin.ConsoleWrite("^b^1ADMIN MUTE >^0^n " + player.FullName + " tripped the bad language filter! ");
 		plugin.Log("Logs/InsaneLimits_bad_words.log", plugin.R("[" + fancy_date + "][" + fancy_time + "] %p_n% said: [" + player.LastChat + "]"));
 	}
 	else if (count == 2) {
-		msg = plugin.R("/@mute " + player.Name + " You have been muted for using profanity in chat!");
+		msg = plugin.R("/@mute " + player.Name + " You have been muted for using bad language in chat!");
 		plugin.ServerCommand("admin.say", msg);
 		plugin.SendPlayerYell(player.Name, msg, 20);
 		plugin.SendGlobalMessage(msg);
 		plugin.PRoConChat("ADMIN MUTE > " + msg);
-		plugin.ConsoleWrite("^b^1ADMIN MUTE >^0^n " + player.FullName + " has been muted for profanity! ");
+		plugin.ConsoleWrite("^b^1ADMIN MUTE >^0^n " + player.FullName + " has been muted for bad language! ");
 		plugin.Log("Logs/InsaneLimits_bad_words.log", plugin.R("[" + fancy_date + "][" + fancy_time + "] %p_n% said: [" + player.LastChat + "]"));
 	}
 
