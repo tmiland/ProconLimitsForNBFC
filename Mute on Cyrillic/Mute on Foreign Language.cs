229557b1ddfe2e7e047c99ac2472f7ed2e3d59dd
Tommy Miland <tmiland@gmail.com>
2014-11-06 20:45:55 +0100

    New name

--- Mute on Cyrillic/Mute on Foreign Language.cs
+++ Mute on Cyrillic/Mute on Foreign Language.cs
@@ -1,9 +1,9 @@
-/* Mute on Cyrillic Letters
+/* Mute on Foreign language
 Set limit to evaluate OnAnyChat, set action to None
 
 Set first_check to this Code: */
 
-	String[] chat_words = Regex.Split(player.LastChat, @"");
+	String[] chat_words = Regex.Split(player.LastChat, @" ");
 
 	foreach(string chat_word in chat_words)
 	{
@@ -22,21 +22,21 @@ Set first_check to this Code: */
 	String msg = "none";
 
 	if (count == 1) {
-		msg = plugin.R("ATTENTION %k_n%! Please avoid Cyrillic, you will be muted for the rest of the round!");
+		msg = plugin.R("ATTENTION %k_n%! English and Scandinavian only in chat!");
 		plugin.SendPlayerYell(player.Name, msg, 20);
 		plugin.SendGlobalMessage(msg);
 		plugin.PRoConChat("ADMIN MUTE > " + msg);
-		plugin.ConsoleWrite("^b^1ADMIN MUTE >^0^n " + player.FullName + " tripped the Cyrillic filter! ");
-		plugin.Log("Logs/InsaneLimits_cyrillic.log", plugin.R("[" + fancy_date + "][" + fancy_time + "] %p_n% said: [" + player.LastChat + "]"));
+		plugin.ConsoleWrite("^b^1ADMIN MUTE >^0^n " + player.FullName + " tripped the Foreigner filter! ");
+		plugin.Log("Logs/InsaneLimits_foreign.log", plugin.R("[" + fancy_date + "][" + fancy_time + "] %p_n% said: [" + player.LastChat + "]"));
 	}
 	else if (count == 2) {
-		msg = plugin.R("/@mute " + player.Name + " You have been muted for Cyrillic letters in chat!");
+		msg = plugin.R("/@mute " + player.Name + " You have been muted for Foreign language in chat!");
 		plugin.ServerCommand("admin.say", msg);
 		plugin.SendPlayerYell(player.Name, msg, 20);
 		plugin.SendGlobalMessage(msg);
 		plugin.PRoConChat("ADMIN MUTE > " + msg);
-		plugin.ConsoleWrite("^b^1ADMIN MUTE >^0^n " + player.FullName + " has been muted for Cyrillic letters in chat! ");
-		plugin.Log("Logs/InsaneLimits_cyrillic.log", plugin.R("[" + fancy_date + "][" + fancy_time + "] %p_n% said: [" + player.LastChat + "]"));
+		plugin.ConsoleWrite("^b^1ADMIN MUTE >^0^n " + player.FullName + " has been muted for Foreign language in chat! ");
+		plugin.Log("Logs/InsaneLimits_foreign.log", plugin.R("[" + fancy_date + "][" + fancy_time + "] %p_n% said: [" + player.LastChat + "]"));
 	}
 
 	return false;
