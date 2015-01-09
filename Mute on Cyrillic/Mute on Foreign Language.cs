de932dc38b010b169c14620758f54d18f2c19b96
Tommy Miland <tmiland@gmail.com>
2015-01-09 10:50:43 +0100

    Check for 3 spaces or more
    Probably Cyrillic

--- Mute on Cyrillic/Mute on Foreign Language.cs
+++ Mute on Cyrillic/Mute on Foreign Language.cs
@@ -3,13 +3,17 @@ Set limit to evaluate OnAnyChat, set action to None
 
 Set first_check to this Code: */
 
-	String[] chat_words = Regex.Split(player.LastChat, @" ");
+	String[] chat_words = Regex.Split(player.LastChat, @"   ");
 
 	foreach(String chat_word in chat_words)
 	{
 	if (String.IsNullOrEmpty(chat_word))
 		{
 			return true;
+		}
+	else if (!String.IsNullOrEmpty(chat_word))
+		{
+			return false;
 		}
 	}
 	return false;
@@ -22,21 +26,29 @@ Set first_check to this Code: */
 	String msg = "none";
 
 	if (count == 1) {
-		msg = plugin.R("ATTENTION %k_n%! English and Scandinavian only in chat!");
+		msg = plugin.R("ATTENTION %k_n%! You entered 3 blank spaces in chat, write some letters!");
 		plugin.SendPlayerYell(player.Name, msg, 20);
 		plugin.SendGlobalMessage(msg);
 		plugin.PRoConChat("ADMIN MUTE > " + msg);
-		plugin.ConsoleWrite("^b^1ADMIN MUTE >^0^n " + player.FullName + " tripped the Foreigner filter! ");
-		plugin.Log("Logs/InsaneLimits_foreign.log", plugin.R("[" + fancy_date + "][" + fancy_time + "] %p_n% said: [" + player.LastChat + "]"));
+		plugin.ConsoleWrite("^b^1ADMIN MUTE >^0^n " + player.FullName + " tripped the Foreign language filter! ");
+		plugin.Log("Logs/InsaneLimits_Foreign.language.log", plugin.R("[" + fancy_date + "][" + fancy_time + "] %p_n% said: [" + player.LastChat + "]"));
 	}
-	else if (count == 2) {
-		msg = plugin.R("/@mute " + player.Name + " You have been muted for Foreign language in chat!");
+	if (count == 2) {
+		msg = plugin.R("ATTENTION %k_n%! ENGLISH/SCANDINAVIAN only in chat, CYRILLIC is not allowed!");
+		plugin.SendPlayerYell(player.Name, msg, 20);
+		plugin.SendGlobalMessage(msg);
+		plugin.PRoConChat("ADMIN MUTE > " + msg);
+		plugin.ConsoleWrite("^b^1ADMIN MUTE >^0^n " + player.FullName + " tripped the Foreign language filter! ");
+		plugin.Log("Logs/InsaneLimits_Foreign.language.log", plugin.R("[" + fancy_date + "][" + fancy_time + "] %p_n% said: [" + player.LastChat + "]"));
+	}
+	else if (count == 3) {
+		msg = plugin.R("/@mute " + player.Name + " Have been MUTED for NOT speaking ENGLISH/SCANDINAVIAN in chat!");
 		plugin.ServerCommand("admin.say", msg);
 		plugin.SendPlayerYell(player.Name, msg, 20);
 		plugin.SendGlobalMessage(msg);
 		plugin.PRoConChat("ADMIN MUTE > " + msg);
-		plugin.ConsoleWrite("^b^1ADMIN MUTE >^0^n " + player.FullName + " has been muted for Foreign language in chat! ");
-		plugin.Log("Logs/InsaneLimits_foreign.log", plugin.R("[" + fancy_date + "][" + fancy_time + "] %p_n% said: [" + player.LastChat + "]"));
+		plugin.ConsoleWrite("^b^1ADMIN MUTE >^0^n " + player.FullName + " has been muted for ENGLISH/SCANDINAVIAN only in chat! ");
+		plugin.Log("Logs/InsaneLimits_Foreign.language.log", plugin.R("[" + fancy_date + "][" + fancy_time + "] %p_n% said: [" + player.LastChat + "]"));
 	}
 
 	return false;
